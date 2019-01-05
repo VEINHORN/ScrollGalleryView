3a090df59acf37ca713e7bccc3789c0945f8d112
Eipeks <apexzdgr8est@gmail.com>
2019-01-05 13:03:54 +0100

    Fresco loader added as another type of Image loader

---
+++ frescoloader/src/main/java/com/ayokunlepaul/scrollgalleryview/loader/fresco/FrescoImageLoader.java
@@ -0,0 +1,110 @@
+package com.ayokunlepaul.scrollgalleryview.loader.fresco;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.widget.ImageView;
+
+import com.facebook.common.executors.UiThreadImmediateExecutorService;
+import com.facebook.common.references.CloseableReference;
+import com.facebook.datasource.BaseDataSubscriber;
+import com.facebook.datasource.DataSource;
+import com.facebook.datasource.DataSubscriber;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.common.ResizeOptions;
+import com.facebook.imagepipeline.core.ImagePipeline;
+import com.facebook.imagepipeline.image.CloseableBitmap;
+import com.facebook.imagepipeline.image.CloseableImage;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.ayokunlepaul.scrollgalleryview.loader.MediaLoader;
+
+/**
+ * Created by ayokunlepaul on 05/01/2019.
+ */
+public class FrescoImageLoader implements MediaLoader {
+
+    private String url;
+    private Integer thumbnailWidth;
+    private Integer thumbnailHeight;
+
+    public FrescoImageLoader(String url) {
+        this.url = url;
+    }
+
+    public FrescoImageLoader(String url, Integer thumbnailWidth, Integer thumbnailHeight) {
+        this.url = url;
+        this.thumbnailWidth = thumbnailWidth;
+        this.thumbnailHeight = thumbnailHeight;
+    }
+
+    @Override
+    public boolean isImage() {
+        return true;
+    }
+
+    @Override
+    public void loadMedia(Context context, final ImageView imageView, SuccessCallback callback) {
+        if (!Fresco.hasBeenInitialized()) {
+            Fresco.initialize(context);
+        }
+        ImagePipeline pipeline = Fresco.getImagePipeline();
+        DataSubscriber subscriber = getSubscriber(imageView, callback);
+        DataSource<CloseableReference<CloseableImage>> dataSource = pipeline.fetchDecodedImage(createImageRequest(), context);
+        dataSource.subscribe(subscriber, UiThreadImmediateExecutorService.getInstance());
+    }
+
+    @Override
+    public void loadThumbnail(Context context, ImageView thumbnailView, SuccessCallback callback) {
+        if (!Fresco.hasBeenInitialized()){
+            Fresco.initialize(context);
+        }
+        ImagePipeline pipeline = Fresco.getImagePipeline();
+        DataSubscriber subscriber = getSubscriber(thumbnailView, callback);
+        DataSource<CloseableReference<CloseableImage>> dataSource = pipeline.fetchDecodedImage(createImageRequest(thumbnailWidth, thumbnailHeight), context);
+        dataSource.subscribe(subscriber, UiThreadImmediateExecutorService.getInstance());
+    }
+
+    private ImageRequest createImageRequest() {
+        return ImageRequest.fromUri(Uri.parse(url));
+    }
+
+    private ImageRequest createImageRequest(Integer thumbnailWidth, Integer thumbnailHeight) {
+        ImageRequestBuilder builder = ImageRequestBuilder.newBuilderWithSource(Uri.parse(url));
+        builder.setResizeOptions(new ResizeOptions(thumbnailWidth, thumbnailHeight));
+
+        return builder.build();
+    }
+
+    private DataSubscriber getSubscriber(final ImageView imageView, final SuccessCallback callback) {
+        return new BaseDataSubscriber<CloseableReference<CloseableBitmap>>() {
+            @Override
+            protected void onNewResultImpl(DataSource<CloseableReference<CloseableBitmap>> dataSource) {
+                if (!dataSource.isFinished()) {
+                    return;
+                }
+                CloseableReference<CloseableBitmap> result = dataSource.getResult();
+                if (result != null) {
+                    final CloseableReference<CloseableBitmap> resultCopy = result.clone();
+                    try {
+                        CloseableBitmap closeableBitmap = resultCopy.get();
+                        Bitmap bitmap = closeableBitmap.getUnderlyingBitmap();
+                        if (bitmap != null && !bitmap.isRecycled()) {
+                            imageView.setImageBitmap(bitmap);
+                            callback.onSuccess();
+                        }
+                    } finally {
+                        result.close();
+                        resultCopy.close();
+                    }
+                }
+            }
+
+            @Override
+            protected void onFailureImpl(DataSource<CloseableReference<CloseableBitmap>> dataSource) {
+                Throwable cause = dataSource.getFailureCause();
+                if (cause != null) cause.printStackTrace();
+            }
+        };
+    }
+}
---
+++ library/src/main/java/com/ayokunlepaul/scrollgalleryview/ImageFragment.java
@@ -0,0 +1,81 @@
+package com.ayokunlepaul.scrollgalleryview;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.chrisbanes.photoview.PhotoView;
+import com.ayokunlepaul.scrollgalleryview.loader.MediaLoader;
+
+/**
+ * Created by veinhorn on 29.8.15.
+ */
+public class ImageFragment extends Fragment {
+
+    private MediaInfo mMediaInfo;
+
+    private HackyViewPager viewPager;
+    private PhotoView photoView;
+    private ScrollGalleryView.OnImageClickListener onImageClickListener;
+
+    public void setOnImageClickListener(ScrollGalleryView.OnImageClickListener onImageClickListener) {
+        this.onImageClickListener = onImageClickListener;
+    }
+
+    public void setMediaInfo(MediaInfo mediaInfo) {
+        mMediaInfo = mediaInfo;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View rootView = inflater.inflate(R.layout.image_fragment, container, false);
+        photoView = rootView.findViewById(R.id.photoView);
+        if (onImageClickListener != null) {
+            photoView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onImageClickListener.onClick();
+                }
+            });
+        }
+        viewPager = (HackyViewPager) getActivity().findViewById(R.id.viewPager);
+
+        if (savedInstanceState != null) {
+            boolean isLocked = savedInstanceState.getBoolean(Constants.IS_LOCKED, false);
+            viewPager.setLocked(isLocked);
+        }
+
+        loadImageToView();
+
+        return rootView;
+    }
+
+    private void loadImageToView() {
+        if (mMediaInfo != null) {
+            mMediaInfo.getLoader().loadMedia(getActivity(), photoView, new MediaLoader.SuccessCallback() {
+                @Override
+                public void onSuccess() {}
+            });
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        if (isViewPagerActive()) {
+            outState.putBoolean(Constants.IS_LOCKED, viewPager.isLocked());
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    private boolean isViewPagerActive() {
+        return viewPager != null;
+    }
+
+    private boolean isBackgroundImageActive() {
+        return photoView != null && photoView.getDrawable() != null;
+    }
+}
