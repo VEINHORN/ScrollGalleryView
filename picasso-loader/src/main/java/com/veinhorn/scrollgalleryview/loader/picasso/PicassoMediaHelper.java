75ca926f738cddeddf75087fa74eeaa2da755a2a
Boris Korogvich <b.korogvich@gmail.com>
2019-01-26 20:15:30 +0300

    update Picasso DSL API

--- picasso-loader/src/main/java/com/veinhorn/scrollgalleryview/loader/picasso/PicassoMediaHelper.java
+++ picasso-loader/src/main/java/com/veinhorn/scrollgalleryview/loader/picasso/PicassoMediaHelper.java
@@ -1,11 +1,11 @@
 package com.veinhorn.scrollgalleryview.loader.picasso;
 
 import com.veinhorn.scrollgalleryview.MediaInfo;
-import com.veinhorn.scrollgalleryview.builder.MediaHelper;
+import com.veinhorn.scrollgalleryview.builder.BasicMediaHelper;
 
-public class PicassoMediaHelper implements MediaHelper {
+public class PicassoMediaHelper extends BasicMediaHelper {
     @Override
-    public MediaInfo media(String url) {
+    public MediaInfo image(String url) {
         return MediaInfo.mediaLoader(new PicassoImageLoader(url));
     }
 }
--- picasso-loader/src/main/java/com/veinhorn/scrollgalleryview/loader/picasso/dsl/DSL.java
+++ picasso-loader/src/main/java/com/veinhorn/scrollgalleryview/loader/picasso/dsl/DSL.java
@@ -1,13 +1,16 @@
 package com.veinhorn.scrollgalleryview.loader.picasso.dsl;
 
 import com.veinhorn.scrollgalleryview.MediaInfo;
-import com.veinhorn.scrollgalleryview.dsl.BasicDSL;
 import com.veinhorn.scrollgalleryview.loader.picasso.PicassoMediaHelper;
 
-public class DSL extends BasicDSL {
+public class DSL {
     private static PicassoMediaHelper mediaHelper = new PicassoMediaHelper();
 
-    public static MediaInfo media(String url) {
-        return mediaHelper.media(url);
+    public static MediaInfo image(String url) {
+        return mediaHelper.image(url);
+    }
+
+    public static MediaInfo video(String url, int placeholderViewId) {
+        return mediaHelper.video(url, placeholderViewId);
     }
 }
