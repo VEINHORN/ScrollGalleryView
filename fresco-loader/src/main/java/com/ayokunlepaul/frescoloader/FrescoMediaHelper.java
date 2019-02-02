f3494549d0b3833f817d99757a69260b56dd9e30
Boris Korogvich <b.korogvich@gmail.com>
2019-02-02 18:31:37 +0300

    improve fresco-loader DSL

--- fresco-loader/src/main/java/com/ayokunlepaul/frescoloader/FrescoMediaHelper.java
+++ fresco-loader/src/main/java/com/ayokunlepaul/frescoloader/FrescoMediaHelper.java
@@ -3,9 +3,33 @@ package com.ayokunlepaul.frescoloader;
 import com.veinhorn.scrollgalleryview.MediaInfo;
 import com.veinhorn.scrollgalleryview.builder.BasicMediaHelper;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FrescoMediaHelper extends BasicMediaHelper {
     @Override
     public MediaInfo image(String url) {
+        return MediaInfo.mediaLoader(new FrescoImageLoader(url));
+    }
+
+    @Override
+    public List<MediaInfo> images(List<String> urls) {
+        List<MediaInfo> medias = new ArrayList<>();
+
+        for (String url : urls) {
+            medias.add(mediaInfo(url));
+        }
+
+        return medias;
+    }
+
+    @Override
+    public List<MediaInfo> images(String... urls) {
+        return images(Arrays.asList(urls));
+    }
+
+    private MediaInfo mediaInfo(String url) {
         return MediaInfo.mediaLoader(new FrescoImageLoader(url));
     }
 }
--- fresco-loader/src/main/java/com/ayokunlepaul/frescoloader/dsl/DSL.java
+++ fresco-loader/src/main/java/com/ayokunlepaul/frescoloader/dsl/DSL.java
@@ -3,11 +3,21 @@ package com.ayokunlepaul.frescoloader.dsl;
 import com.ayokunlepaul.frescoloader.FrescoMediaHelper;
 import com.veinhorn.scrollgalleryview.MediaInfo;
 
+import java.util.List;
+
 public class DSL {
     private static FrescoMediaHelper mediaHelper = new FrescoMediaHelper();
 
     public static MediaInfo image(String url) {
         return mediaHelper.image(url);
+    }
+
+    public static List<MediaInfo> images(List<String> urls) {
+        return mediaHelper.images(urls);
+    }
+
+    public static List<MediaInfo> images(String... urls) {
+        return mediaHelper.images(urls);
     }
 
     public static MediaInfo video(String url, int placeholderViewId) {
